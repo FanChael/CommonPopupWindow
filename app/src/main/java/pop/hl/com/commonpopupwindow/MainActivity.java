742e7b0866a62fdfb122fa05b5c5f0ffc7565b21
huanglei <huangleililei@gmail.com>
2019-03-11 12:24:01 +0800

    解决一下内容包裹问题；

--- app/src/main/java/pop/hl/com/commonpopupwindow/MainActivity.java
+++ app/src/main/java/pop/hl/com/commonpopupwindow/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    //public native String stringFromJNI();
 
     private BasePop.Builder builder;
     private AlertDialog alertDialog;
